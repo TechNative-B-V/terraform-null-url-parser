48ee0a01fa7a5adf6214850ab262e2282927fa8a
Pim Snel <post@pimsnel.com>
2023-10-16 15:01:09 +0200

    force failing test

--- test/url_parser_test.go
+++ test/url_parser_test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	
 	"testing"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
@@ -19,7 +18,8 @@ func TestUrl(t *testing.T) {
 
 	terraform.InitAndApply(t, terraformOptions)
 
-	input_url := terraform.Output(t, terraformOptions, "input_url")
+//	input_url := terraform.Output(t, terraformOptions, "input_url")
+	input_url := "https://example.io/namespaces/TechNative-B-Vxxx"
 	output_url := terraform.Output(t, terraformOptions, "output_url")
 
 	assert.Equal(t, input_url, output_url)
